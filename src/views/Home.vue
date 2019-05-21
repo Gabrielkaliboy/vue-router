3553553ec6fc918fec6d70b9875b03ebde7b0b2f
company_gabriel <Gabriel_wei@163.com>
2019-05-21 18:07:37 +0800

    去除editorconfig,push跳转

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <p>home</p>
+    <button @click="btn1">点击我会跳转到About</button>
+    <button @click="btn2">点击我会跳转到About</button>
   </div>
 </template>
 
@@ -17,6 +19,16 @@ export default {
   methods: {
     goBack: function () {
       window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
+    },
+    btn1: function () {
+      // 可以是字符串
+      this.$router.push('about')
+    },
+    btn2: function () {
+      // 可以是对象
+      this.$router.push({
+        path: 'home'
+      })
     }
   }
 }
